1f68cbc47a9159a36f5e8516db36be6e1ea362cb
adonisrods <rodriguesadonis25@gmail.com>
2022-10-18 17:32:52 +0530

    added

--- formbooking/settings.py
+++ formbooking/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g_so0-=o%yp55b=ch)amgw1o@b#m6chb!$00vnpg^95q=&tqvn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['192.168.0.169']
 
 
 # Application definition
