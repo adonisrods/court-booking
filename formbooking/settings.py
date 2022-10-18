4cf2108a54849b93550eec9bfd14fbf3e382d07c
adonisrods <rodriguesadonis25@gmail.com>
2022-10-18 16:39:21 +0530

    added static root

--- formbooking/settings.py
+++ formbooking/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'accounts/static/')
 STATIC_URL = 'accounts/static/'
 
 # Default primary key field type
