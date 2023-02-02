8101ccb1ca1613c049cb7dd187ec2bea03419de9
adonisrods <63241940+adonisrods@users.noreply.github.com>
2023-02-02 12:19:44 +0530

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import User,booking_info
 # Register your models here.
-admin.site.register(User)
-admin.site.register(booking_info)
+class UserAdmin(admin.ModelAdmin):
+    list_display= ('phone_number', 'username', 'is_phone_verified', 'email')
+
+class booking_infoAdmin(admin.ModelAdmin):
+    list_display= ('date', 'is_booked', 'phone_no_registered', 'slot_time', 'ground_name')
+#hi test
+
+admin.site.register(User,UserAdmin)
+admin.site.register(booking_info,booking_infoAdmin)
